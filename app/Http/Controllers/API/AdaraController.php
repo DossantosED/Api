b2e56a26f79e0cf01a0c653dcebe84bb9b10dee4
Emanuel Dos Santos <57354733+DossantosED@users.noreply.github.com>
2021-07-24 00:18:27 -0300

    mejoro logica de chatBot

--- app/Http/Controllers/API/AdaraController.php
+++ app/Http/Controllers/API/AdaraController.php
@@ -19,58 +19,71 @@ class AdaraController extends Controller
         return response(['response' => $response, 200]);
     }
 
-    private function searchMessage($message, $user){
-        $res = "Lo siento no te entiendo";
-        $msg = strtolower($message);
-        $msg = preg_replace("/(.)\\1+/", "$1", $msg);
-        $msg = explode(" ",$msg);
-        $msg = array_unique($msg);
-        $nombre = ["nombre", "lamas"];
-        $eres = ["que", "quien", "eres"];
-        $saludar = ["hola"];
-        $cantSaludar = 0;
-        $cantNombre = 0;
-        $cantEres = 0;
-        $dictionary = [$saludar, $nombre, $eres];
-        $saludarResponse = [ucfirst($saludar[0]).' '.$user."!", ucfirst($saludar[0]).' '.$user."! en que puedo ayudarte?", ucfirst($saludar[0]).' '.$user."! cómo estás hoy?"];
-        $nombreResponse = ["Mi nombre es Adara", "Me llamo Adara"];
-        $eresResponse = ["Soy una IA", "Soy una Inteligencia Artificial"];
-        foreach ($dictionary as $d){
-            for ($i = 0; $i< count($d); $i++){
-                for($k = 0; $k < count($msg); $k++){
-                    if( str_contains($d[$i],$msg[$k]) && $d == $saludar){
-                        if($d[$i] === $msg[$k]){
-                            $cantSaludar++;
-                        }
-                        $cantSaludar = $cantSaludar + strlen($msg[$k]);
-                    }else if(str_contains($d[$i],$msg[$k]) && $d == $nombre){
-                        if($d[$i] === $msg[$k]){
-                            $cantNombre++;
-                        }
-                        $cantNombre = $cantNombre + strlen($msg[$k]);
-                    }else if(str_contains($d[$i],$msg[$k]) && $d == $eres){
-                        if($d[$i] === $msg[$k]){
-                            $cantEres++;
-                        }
-                        $cantEres = $cantEres + strlen($msg[$k]);
-                    }
+
+    private function searchMessage($input, $user)
+    {
+        $msg = $this->inputFormat($input);
+        $name = ["nombre", "lamas"];
+        $be = ["que", "quien", "eres"];
+        $greet = ["hola"];
+        $cantGreet = 0;
+        $cantName = 0;
+        $cantBe = 0;
+        $dictionary = ['Greet' => $greet, 'Name' => $name, 'Be' => $be];
+        $sizeMsg = count($msg);
+        
+        foreach ($dictionary as $j => $d){
+            $sizeDictionary = count($d);
+            for ($i = 0; $i < $sizeDictionary; $i++){
+                for($k = 0; $k < $sizeMsg; $k++){
+                    ${'cant'.$j} = ${'cant'.$j} + $this->calculateHeft($msg[$k], $d[$i]);
                 }
             }
         }
-        if($cantSaludar > $cantEres && $cantSaludar > $cantNombre){
-            $res = $saludarResponse[array_rand($saludarResponse)];
-        }else if($cantEres > $cantSaludar && $cantEres > $cantNombre){
-            $res = $eresResponse[array_rand($eresResponse)];
-        }else if($cantNombre > 0){
-            $res = $nombreResponse[array_rand($nombreResponse)];
+
+        return $this->getResponse($cantGreet, $cantBe, $cantName, $user, $greet);
+    }
+
+    private function inputFormat($input)
+    {
+        $convertLowerInput = strtolower($input);
+        $delWordsRepeat = preg_replace("/(.)\\1+/", "$1", $convertLowerInput);
+        $splitBySpaces = explode(" ",$delWordsRepeat);
+        return array_unique($splitBySpaces);
+    }
+
+    private function calculateHeft($input, $vec)
+    {
+        $heft = 0;
+        if( str_contains($vec,$input) ){
+            $heft = $heft + strlen($input);
+            $vec === $input ? $heft++ : $heft;
         }
-        return $res;
+        return $heft;
+    }
+
+    private function getResponse($cantGreet, $cantBe, $cantName, $user, $greet)
+    {
+        $response = "Lo siento no te entiendo";
+        $greetResponse = [ucfirst($greet[0]).' '.$user."!", ucfirst($greet[0]).' '.$user."! en que puedo ayudarte?", ucfirst($greet[0]).' '.$user."! cómo estás hoy?"];
+        $nameResponse = ["Mi nombre es Adara", "Me llamo Adara"];
+        $beResponse = ["Soy una IA", "Soy una Inteligencia Artificial"];
+        
+        if($cantGreet > $cantBe && $cantGreet > $cantName){
+            $response = $greetResponse[array_rand($greetResponse)];
+        }else if($cantBe > $cantGreet && $cantBe > $cantName){
+            $response = $beResponse[array_rand($beResponse)];
+        }else if($cantName > 0){
+            $response = $nameResponse[array_rand($nameResponse)];
+        }
+        return $response;
     }
 
     /**
      * 
      */
-    public function token(){
+    public function token()
+    {
         $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-°!"#%&/()|~=%$?¡][*¨{ñ´+:.;,¿]';
         $charactersLength = strlen($characters);
         $randomString = '';
